75172c7fd97d8039a639bf7b11597ce5c873cafa
Arya Yudha P <aryayudha2109@gmail.com>
2025-03-05 23:23:42 +0700

    Membuat layout untuk tampilan

--- app/src/main/java/dev/yudha/kabaddiprojek/MainActivity.kt
+++ app/src/main/java/dev/yudha/kabaddiprojek/MainActivity.kt
@@ -1,20 +1,52 @@
 package dev.yudha.kabaddiprojek
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var scoreA = 0
+    private var scoreB = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val scoreTeamA: TextView = findViewById(R.id.scoreTeamA)
+        val scoreTeamB: TextView = findViewById(R.id.scoreTeamB)
+
+        val btnAdd1TeamA: Button = findViewById(R.id.btnAdd1TeamA)
+        val btnAdd2TeamA: Button = findViewById(R.id.btnAdd2TeamA)
+        val btnAdd1TeamB: Button = findViewById(R.id.btnAdd1TeamB)
+        val btnAdd2TeamB: Button = findViewById(R.id.btnAdd2TeamB)
+        val btnReset: Button = findViewById(R.id.btnReset)
+
+        btnAdd1TeamA.setOnClickListener {
+            scoreA += 1
+            scoreTeamA.text = scoreA.toString()
+        }
+
+        btnAdd2TeamA.setOnClickListener {
+            scoreA += 2
+            scoreTeamA.text = scoreA.toString()
+        }
+
+        btnAdd1TeamB.setOnClickListener {
+            scoreB += 1
+            scoreTeamB.text = scoreB.toString()
+        }
+
+        btnAdd2TeamB.setOnClickListener {
+            scoreB += 2
+            scoreTeamB.text = scoreB.toString()
+        }
+
+        btnReset.setOnClickListener {
+            scoreA = 0
+            scoreB = 0
+            scoreTeamA.text = scoreA.toString()
+            scoreTeamB.text = scoreB.toString()
         }
     }
 }
